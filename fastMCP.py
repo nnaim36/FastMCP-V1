1aad0f65f243d9b56419fa88c266f8a4350b080e
Nile <nnaim36@gmail.com>
2025-06-28 19:44:46 -0400

    set up some basic agents

--- fastMCP.py
+++ fastMCP.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastmcp import FastMCP
+from datetime import datetime
+from zoneinfo import ZoneInfo
 import requests
 import os
 
@@ -20,5 +22,23 @@ def personalized_greeting(name: str) -> str:
     """Generates a personalized greeting for the given name."""
     return f"Hello, {name}! Welcome to the MCP server."
 
-if __name__ == "__main__":
-    mcp.run()
+@mcp.get("/get-local-time")
+def get_local_time(timezone: str = "UTC") -> str:
+    """
+    Return the current time for the time zone tha the user is in
+    the backup return will be universal time(UTC).
+    """
+    try:
+        now = datetime.now(ZoneInfo(timezone))
+        return now.isoformat()
+    except Exception:
+        now = datetime.now(datetime.timezone.utc)
+        return now.isoformat() + "z"
+
+
+@mcp.get("get-location")
+def getlocation()->str:
+    """
+    receive the users location.
+    """
+    return "we have not figure this one out yet."
